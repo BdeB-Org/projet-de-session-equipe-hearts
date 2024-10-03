384a3492d3ca6325a652281c5687064ee0e309b9
houssem342503 <2253158@bdeb.qc.ca>
2024-10-03 17:33:22 -0400

    amelioration de la page acceuil
    .

--- website/server.js
+++ website/server.js
@@ -32,29 +32,29 @@ const server = app.listen(4000, function () {
 /*
     Connect MySql
 */
- const con = mysql.createConnection({
-     host: "localhost",
-     user: "scott",
-     password: "oracle",
-    database: "myhearts"
- });
+//  const con = mysql.createConnection({
+//      host: "localhost",
+//      user: "scott",
+//      password: "oracle",
+//     database: "myhearts"
+//  });
 
- app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-app.use(session({
-    secret: 'your-secret-key', 
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false } 
-}));
+//  app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 
- con.connect(function (err) {
-    if (err) throw err;
-    console.log("connected!");
- });
+// app.use(session({
+//     secret: 'your-secret-key', 
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { secure: false } 
+// }));
+
+
+//  con.connect(function (err) {
+//     if (err) throw err;
+//     console.log("connected!");
+//  });
 
 
 /*
@@ -74,7 +74,7 @@ app.get("/", function (req, res) {
     res.render("pages/accueil", {
         siteTitle: "Index",
         pageTitle: "index",
-        userDetails: req.session.user,
+        // userDetails: req.session.user,
     });
 });
 
@@ -82,7 +82,7 @@ app.get("/event/inscription", function (req, res) {
     res.render("pages/inscription", {
         siteTitle: "Connexion",
         pageTitle: "Connectez-vous",
-        userDetails: req.session.user,
+        // userDetails: req.session.user,
     });
 });
 
@@ -90,7 +90,7 @@ app.get("/event/creationCompte", function (req, res) {
     res.render("pages/creationCompte", {
         siteTitle: "Créer Compte",
         pageTitle: "Créer Compte",
-        userDetails: req.session.user,
+        // userDetails: req.session.user,
 
     });
 });
@@ -99,7 +99,7 @@ app.get("/event/abonnement", function (req, res) {
     res.render("pages/abonnement", {
         siteTitle: "Abonnez-Vous",
         pageTitle: "Abonnez-Vous",
-        userDetails: req.session.user,
+        // userDetails: req.session.user,
 
     });
 });
@@ -108,7 +108,7 @@ app.get("/event/apropos", function (req, res) {
     res.render("pages/apropos", {
         siteTitle: "Aprpoos",
         pageTitle: "A Propos",
-        userDetails: req.session.user,
+        // userDetails: req.session.user,
 
     });
 });
@@ -119,34 +119,34 @@ app.get("/event/apropos", function (req, res) {
 */
 
 
-app.post('/event/creationCompte', (req, res) => {
-    const { email, password } = req.body;
+// app.post('/event/creationCompte', (req, res) => {
+//     const { email, password } = req.body;
 
 
-    const verifyUserQuery = "SELECT * FROM e_utilisateur WHERE E_COURRIEL = ?";
-    con.query(verifyUserQuery, [email], (err, result) => {
-        if (err) {
-            console.error("Error verifying user:", err);
-            return res.status(500).send("Internal Server Error");
-        }
+//     const verifyUserQuery = "SELECT * FROM e_utilisateur WHERE E_COURRIEL = ?";
+//     con.query(verifyUserQuery, [email], (err, result) => {
+//         if (err) {
+//             console.error("Error verifying user:", err);
+//             return res.status(500).send("Internal Server Error");
+//         }
 
-        if (result.length > 0) {
-            return res.status(400).send("Email already in use");
-        }
-        bcrypt.hash(password, 10, (hashErr, hashedPassword) => {
-            if (hashErr) {
-                console.error("Error hashing password:", hashErr);
-                return res.status(500).send("Internal Server Error");
-            }
+//         if (result.length > 0) {
+//             return res.status(400).send("Email already in use");
+//         }
+//         bcrypt.hash(password, 10, (hashErr, hashedPassword) => {
+//             if (hashErr) {
+//                 console.error("Error hashing password:", hashErr);
+//                 return res.status(500).send("Internal Server Error");
+//             }
 
-            const insertUserQuery = "INSERT INTO e_utilisateur (E_COURRIEL, E_PASSWORD) VALUES (?, ?)";
-            con.query(insertUserQuery, [email, hashedPassword], (insertErr) => {
-                if (insertErr) {
-                    console.error("Error inserting user:", insertErr);
-                    return res.status(500).send("Internal Server Error");
-                }
-                res.status(201).send("User created successfully");
-            });
-        });
-    });
-});
+//             const insertUserQuery = "INSERT INTO e_utilisateur (E_COURRIEL, E_PASSWORD) VALUES (?, ?)";
+//             con.query(insertUserQuery, [email, hashedPassword], (insertErr) => {
+//                 if (insertErr) {
+//                     console.error("Error inserting user:", insertErr);
+//                     return res.status(500).send("Internal Server Error");
+//                 }
+//                 res.status(201).send("User created successfully");
+//             });
+//         });
+//     });
+// });
